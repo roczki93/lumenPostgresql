32a7f55e5395a156f036943ac5eb3695c096125e
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-20 20:44:06 +0200

    ksksks

--- database/migrations/2017_06_20_173747_user_information.php
+++ database/migrations/2017_06_20_173747_user_information.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class userInformation2 extends Migration
+class userInformation extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class userInformation2 extends Migration
      */
     public function up()
     {
-        Schema::create('user_information2', function(Blueprint $table)
+        Schema::create('user_information', function(Blueprint $table)
         {
             $table->increments('id');
             $table->string('first_name');
@@ -35,7 +35,7 @@ class userInformation2 extends Migration
      */
     public function down()
     {
-        Schema::drop('user_information2');
+        Schema::drop('user_information');
         //
     }
 }
