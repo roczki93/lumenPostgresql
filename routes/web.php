2471c7c9f0d331fae298d713a4c43958f26a544f
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-20 20:12:38 +0200

    ksksks

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@
 // });
 // $app->group(['prefix' => 'api/v1','namespace' => 'App\Http\Controllers'], function($app)
 // {
-$app->get('book','BookController@index');
-$app->post('book','BookController@createBook');
-$app->get('book/{id}','BookController@getbook');
+// $app->get('book','BookController@index');
+// $app->post('book','BookController@createBook');
+// $app->get('book/{id}','BookController@getbook');
 // });
 // $app->get('/', function() use ($app) {
 //     return "Lumen RESTful API By CoderExample (https://coderexample.com)";
@@ -59,3 +59,12 @@ $app->get('book/{id}','BookController@getbook');
       
 //     $app->delete('book/{id}','BookController@deleteBook');
 // });
+    $app->get('UserInfo','UserInfoController@index');
+  
+    $app->get('UserInfo/{id}','UserInfoController@getUserInfo');
+      
+    $app->post('UserInfo','UserInfoController@createUserInfo');
+      
+    $app->put('UserInfo/{id}','UserInfoController@updateUserInfo');
+      
+    $app->delete('UserInfo/{id}','UserInfoController@deleteUserInfo');
