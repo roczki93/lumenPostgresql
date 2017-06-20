1bc2b920fcb469f7e68805e658027399bd307477
root <root@localhost.localdomain>
2017-06-20 18:40:00 +0200

    books

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'CreateBooksTable' => $baseDir . '/database/migrations/2017_06_20_123808_create_books_table.php',
+    'CreateBooksTable–create' => $baseDir . '/database/migrations/2017_06_20_162042_create_books_table–create=books.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'ExampleTest' => $baseDir . '/tests/ExampleTest.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
@@ -540,5 +541,4 @@ return array(
     'SebastianBergmann\\Version' => $vendorDir . '/sebastian/version/src/Version.php',
     'TestCase' => $baseDir . '/tests/TestCase.php',
     'Text_Template' => $vendorDir . '/phpunit/php-text-template/src/Template.php',
-    '–create' => $baseDir . '/database/migrations/2017_06_20_123815_–create=books.php',
 );
