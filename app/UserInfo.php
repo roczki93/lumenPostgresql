9c73b152614b52b220a67dbc70dfdee3cad17b80
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-21 08:48:09 +0200

    fix post/put

--- app/UserInfo.php
+++ app/UserInfo.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserInfo extends Model
 {
-
+        public $timestamps = false;
      protected $fillable = ['first_name', 'last_name', 'email', 'gender', 'city', 'street', 'latitude', 'longitude'];
 
 }
